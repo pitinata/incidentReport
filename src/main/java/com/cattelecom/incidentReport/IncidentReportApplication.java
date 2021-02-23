14bd48d37479449463c5c77002efeb3099194728
DESKTOP-UN83TNR\CAT <pitinat.a@cattelecom.com>
2021-02-23 11:55:50 +0700

    Apply Spring Initializer
    Use spring initializer to create project template.

---
+++ src/main/java/com/cattelecom/incidentReport/IncidentReportApplication.java
@@ -0,0 +1,13 @@
+package com.cattelecom.incidentReport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cattelecom/incidentReport/IncidentReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cattelecom.incidentReport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncidentReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
